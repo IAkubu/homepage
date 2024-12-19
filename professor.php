7ced3c22e12f3deb5e9273e4f604d5a2d3995b51
Reyssan <159106731+ssanrey@users.noreply.github.com>
2024-12-19 10:15:41 -0800

    Update professor.php

--- professor.php
+++ professor.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="This the university database">
         <meta name="keywords" content="">
-        <link rel="stylesheet" href="css/styles.css">
+        <link rel="stylesheet" href="styles.css">
         <title>University</title>
         <style>
             footer{
@@ -19,8 +19,69 @@
         <?php include "includes/nav.php" ?>
         <?php include "includes/header.php" ?>
 
-        <main></main>
+        <h1>List of Professors</h1>
+        <br>
+        <table>
+                <thread>
+                        <tr>
+                                <th>SSN</th>
+                                <th>Name</th>
+                                <th>Street</th>
+                                <th>City</th>
+                                <th>State</th>
+                                <th>Zip</th>
+                                <th>AreaCode</th>
+                                <th>Telephone</th>
+                                <th>Sex</th>
+                                <th>Title</th>
+                                <th>Salary</th>
+                                <th>Degrees</th>
+                        </tr>
+                </thread>
 
+                <tbody>
+                        <?php
+                        $servername = "mariadb";
+                        $username = "";
+                        $password = "";
+                        $database = "";
+
+                        // Create connection
+                        $conn = new mysqli($servername, $username, $password, $database);
+
+                        if ($conn->connect_error) {
+                                die("Connection failed: " . $conn->connect_error);
+                        }
+
+                        // read all row from database table
+                        $sql = "SELECT * FROM Professors";
+                        $result = $conn->query($sql);
+
+                        if (!$result) {
+                                die("Invalid query: " . $conn->error);
+                        }
+
+                        // read data of each row
+                        while($row = $result->fetch_assoc()) {
+                                echo "<tr>
+                                        <td>" . $row[SSN] . "</td>
+                                        <td>" . $row[Name] . "</td>
+                                        <td>" . $row[Street] . "</td>
+                                        <td>" . $row[City] . "</td>
+                                        <td>" . $row[State] . "</td>
+                                        <td>" . $row[Zip] . "</td>
+                                        <td>" . $row[AreaCode] . "</td>
+                                        <td>" . $row[Telephone] . "</td>
+                                        <td>" . $row[Sex] . "</td>
+                                        <td>" . $row[Title] . "</td>
+                                        <td>" . $row[Salary] . "</td>
+                                        <td>" . $row[Degrees] . "</td>
+
+                                </tr>";
+                        }
+                        ?>
+                </tbody>
+        </table>
         <?php include "includes/footer.php" ?>
 
 
