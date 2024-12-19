4bdbe0775ea6c94dd515feafefe7a715eb78e297
Reyssan <159106731+ssanrey@users.noreply.github.com>
2024-12-19 10:37:06 -0800

    Update student.php

--- student.php
+++ student.php
@@ -8,6 +8,22 @@
         <link rel="stylesheet" href="styles.css">
         <title>University</title>
         <style>
+            table{
+                border-collapse: collapse;
+                width: 100%;
+                color: #588c7e;
+                font-family: monospace;
+                font-size: 15px;
+                text-align: left;
+            }
+
+            th{
+                background-color: #588c7e;
+                color: white;
+            }
+
+            tr:nth-child(even) {background-color: #f2f2f2}
+
             footer{
                 position: fixed;
                 bottom: 0
